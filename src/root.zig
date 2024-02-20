da503cf6126b75f6db55f1ea1bc2b3df963a6e11
notsatvrn <satvrn@gmx.com>
2024-02-19 17:09:07 -0800

    ok

--- src/root.zig
+++ src/root.zig
@@ -10,24 +10,27 @@ pub const Codepoint = packed struct {
     // UTF-8
 
     // based on std.unicode.{utf8ByteSequenceLength, utf8Decode}
-    pub fn fromUtf8(utf8: []const u8) !struct { Self, usize } {
+    pub fn fromUtf8(in: []const u8) !struct { Self, usize } {
         var codepoint = Self{};
         var length: usize = 1;
 
-        switch (utf8[0]) {
+        switch (in[0]) {
             0b0000_0000...0b0111_1111 => {
-                codepoint.inner = @as(u21, utf8[0]);
+                codepoint.inner = @as(u21, in[0]);
             },
             0b1100_0000...0b1101_1111 => {
-                codepoint.inner = try std.unicode.utf8Decode2(utf8[0..2]);
+                if (in.len < 2) return error.InvalidCodepoint;
+                codepoint.inner = try std.unicode.utf8Decode2(in[0..2]);
                 length = 2;
             },
             0b1110_0000...0b1110_1111 => {
-                codepoint.inner = try std.unicode.utf8Decode3(utf8[0..3]);
+                if (in.len < 3) return error.InvalidCodepoint;
+                codepoint.inner = try std.unicode.utf8Decode3(in[0..3]);
                 length = 3;
             },
             0b1111_0000...0b1111_0111 => {
-                codepoint.inner = try std.unicode.utf8Decode4(utf8[0..4]);
+                if (in.len < 4) return error.InvalidCodepoint;
+                codepoint.inner = try std.unicode.utf8Decode4(in[0..4]);
                 length = 4;
             },
             else => return error.InvalidStartByte,
@@ -64,33 +67,33 @@ pub const Codepoint = packed struct {
     // Java Modified UTF-8
 
     // based on JNI Type Docs
-    pub fn fromMutf8(mutf8: []const u8) !struct { Self, usize } {
+    pub fn fromMutf8(in: []const u8) !struct { Self, usize } {
         var codepoint = Self{};
         var length: usize = 1;
 
-        if (mutf8 == 0) {
+        if (in == 0) {
             return error.NullByte;
-        } else if (mutf8[0] < 0x80) {
-            codepoint.inner = @as(u21, mutf8[0]);
-        } else if (mutf8.len >= 2 and (mutf8[1] & 0xC0) == 0x80) {
-            if ((mutf8[0] & 0xE0) == 0xC0) {
-                codepoint.inner = (@as(u21, mutf8[0] & 0b11111) << 6) | @as(u21, mutf8[1] & 0b111111);
+        } else if (in[0] < 0x80) {
+            codepoint.inner = @as(u21, in[0]);
+        } else if (in.len >= 2 and (in[1] & 0xC0) == 0x80) {
+            if ((in[0] & 0xE0) == 0xC0) {
+                codepoint.inner = (@as(u21, in[0] & 0b11111) << 6) | @as(u21, in[1] & 0b111111);
                 length = 2;
-            } else if (mutf8.len >= 3 and (mutf8[0] & 0xF0) == 0xE0 and (mutf8[2] & 0xC0) == 0x80) {
-                if (mutf8.len >= 6 and
-                    mutf8[0] == 0b11101101 and
-                    (mutf8[1] & 0xF0) == 0xA0 and
-                    mutf8[3] == 0b11101101 and
-                    (mutf8[4] & 0xF0) == 0xB0 and
-                    (mutf8[5] & 0xC0) == 0x80)
+            } else if (in.len >= 3 and (in[0] & 0xF0) == 0xE0 and (in[2] & 0xC0) == 0x80) {
+                if (in.len >= 6 and
+                    in[0] == 0b11101101 and
+                    (in[1] & 0xF0) == 0xA0 and
+                    in[3] == 0b11101101 and
+                    (in[4] & 0xF0) == 0xB0 and
+                    (in[5] & 0xC0) == 0x80)
                 {
-                    codepoint.inner = (@as(u21, mutf8[1] & 0b1111) << 16) |
-                        (@as(u21, mutf8[2] & 0b111111) << 10) |
-                        (@as(u21, mutf8[4] & 0b1111) << 6) |
-                        @as(u21, mutf8[5] & 0b111111);
+                    codepoint.inner = (@as(u21, in[1] & 0b1111) << 16) |
+                        (@as(u21, in[2] & 0b111111) << 10) |
+                        (@as(u21, in[4] & 0b1111) << 6) |
+                        @as(u21, in[5] & 0b111111);
                     length = 6;
                 } else {
-                    codepoint.inner = (@as(u21, mutf8[0] & 0b1111) << 12) | (@as(u21, mutf8[1] & 0b111111) << 6) | @as(u21, mutf8[2] & 0b111111);
+                    codepoint.inner = (@as(u21, in[0] & 0b1111) << 12) | (@as(u21, in[1] & 0b111111) << 6) | @as(u21, in[2] & 0b111111);
                     length = 3;
                 }
             } else {
@@ -137,17 +140,18 @@ pub const Codepoint = packed struct {
     // UTF-16-LE
 
     // based on std.unicode.utf16DecodeSurrogatePair + wikipedia
-    pub fn fromUtf16le(utf16le: []const u16) !struct { Self, usize } {
+    pub fn fromUtf16le(in: []const u16) !struct { Self, usize } {
         var codepoint = Self{ .codepoint = 0 };
         var length: usize = 1;
 
-        switch (utf16le[0]) {
+        switch (in[0]) {
             0x0000...0xD7FF, 0xE000...0xFFFF => {
-                codepoint.inner = @as(u21, utf16le[0]);
+                codepoint.inner = @as(u21, in[0]);
             },
-            else => if (utf16le[0] & ~@as(u16, 0x03ff) == 0xd800) {
-                if (utf16le[1] & ~@as(u16, 0x03ff) != 0xdc00) return error.ExpectedSecondSurrogateHalf;
-                codepoint.inner = 0x10000 + ((@as(u21, utf16le[0]) & 0x03ff) << 10) | (utf16le[1] & 0x03ff);
+            else => if (in[0] & ~@as(u16, 0x03ff) == 0xd800) {
+                if (in.len < 2) return error.InvalidCodepoint;
+                if (in[1] & ~@as(u16, 0x03ff) != 0xdc00) return error.ExpectedSecondSurrogateHalf;
+                codepoint.inner = 0x10000 + ((@as(u21, in[0]) & 0x03ff) << 10) | (in[1] & 0x03ff);
                 length = 2;
             } else {
                 return error.InvalidStartByte;
@@ -174,14 +178,14 @@ pub const Codepoint = packed struct {
 
 // IMPLEMENTATIONS
 
-inline fn codepointsFromUtf8Alloc(allocator: Allocator, utf8: []const u8) !std.ArrayList(Codepoint) {
-    var codepoints = try std.ArrayList(Codepoint).initCapacity(allocator, utf8.len);
+inline fn codepointsFromUtf8Alloc(allocator: Allocator, in: []const u8) !std.ArrayList(Codepoint) {
+    var codepoints = try std.ArrayList(Codepoint).initCapacity(allocator, in.len);
     errdefer codepoints.deinit();
 
     var i: usize = 0;
     var len: usize = 0;
-    while (i < utf8.len) : (len += 1) {
-        const codepoint = try Codepoint.fromUtf8(utf8[i..]);
+    while (i < in.len) : (len += 1) {
+        const codepoint = try Codepoint.fromUtf8(in[i..]);
         codepoints.appendAssumeCapacity(codepoint[0]);
         i += codepoint[1];
     }
@@ -201,14 +205,41 @@ inline fn codepointsToUtf8Alloc(allocator: Allocator, codepoints: []const Codepo
     return try out.toOwnedSlice();
 }
 
-inline fn codepointsFromUtf16leAlloc(allocator: Allocator, utf16le: []const u16) !std.ArrayList(Codepoint) {
-    var codepoints = try std.ArrayList(Codepoint).initCapacity(allocator, utf16le.len);
+inline fn codepointsFromMutf8Alloc(allocator: Allocator, in: []const u8) !std.ArrayList(Codepoint) {
+    var codepoints = try std.ArrayList(Codepoint).initCapacity(allocator, in.len);
     errdefer codepoints.deinit();
 
     var i: usize = 0;
     var len: usize = 0;
-    while (i < utf16le.len) : (len += 1) {
-        const codepoint = try Codepoint.fromUtf16le(utf16le[i..]);
+    while (i < in.len) : (len += 1) {
+        const codepoint = try Codepoint.fromMutf8(in[i..]);
+        codepoints.appendAssumeCapacity(codepoint[0]);
+        i += codepoint[1];
+    }
+
+    codepoints.shrinkAndFree(len);
+    return codepoints;
+}
+
+inline fn codepointsToMutf8Alloc(allocator: Allocator, codepoints: []const Codepoint) ![]u8 {
+    var out = try std.ArrayList(u8).initCapacity(allocator, codepoints.len);
+    defer out.deinit();
+
+    for (codepoints) |codepoint| {
+        try codepoint.appendToMutf8(&out);
+    }
+
+    return try out.toOwnedSlice();
+}
+
+inline fn codepointsFromUtf16leAlloc(allocator: Allocator, in: []const u16) !std.ArrayList(Codepoint) {
+    var codepoints = try std.ArrayList(Codepoint).initCapacity(allocator, in.len);
+    errdefer codepoints.deinit();
+
+    var i: usize = 0;
+    var len: usize = 0;
+    while (i < in.len) : (len += 1) {
+        const codepoint = try Codepoint.fromUtf16le(in[i..]);
         codepoints.appendAssumeCapacity(codepoint[0]);
         i += codepoint[1];
     }
@@ -247,9 +278,11 @@ pub const MutableString = struct {
         };
     }
 
-    pub fn fromUtf8(allocator: Allocator, utf8: []const u8) !Self {
+    // UTF-8
+
+    pub fn fromUtf8(allocator: Allocator, in: []const u8) !Self {
         return .{
-            .codepoints = try codepointsFromUtf8Alloc(allocator, utf8),
+            .codepoints = try codepointsFromUtf8Alloc(allocator, in),
         };
     }
 
@@ -257,9 +290,23 @@ pub const MutableString = struct {
         return codepointsToUtf8Alloc(self.codepoints.allocator, self.codepoints.items);
     }
 
-    pub fn fromUtf16le(allocator: Allocator, utf16le: []const u16) !Self {
+    // Java Modified UTF-8
+
+    pub fn fromMutf8(allocator: Allocator, in: []const u8) !Self {
         return .{
-            .codepoints = try codepointsFromUtf16leAlloc(allocator, utf16le),
+            .codepoints = try codepointsFromMutf8Alloc(allocator, in),
+        };
+    }
+
+    pub fn toMutf8(self: *const Self) ![]u8 {
+        return codepointsToMutf8Alloc(self.codepoints.allocator, self.codepoints.items);
+    }
+
+    // UTF-16-LE
+
+    pub fn fromUtf16le(allocator: Allocator, in: []const u16) !Self {
+        return .{
+            .codepoints = try codepointsFromUtf16leAlloc(allocator, in),
         };
     }
 
@@ -279,13 +326,15 @@ pub const StaticString = struct {
         };
     }
 
-    pub inline fn fromUtf8Comptime(comptime utf8: []const u8) Self {
-        comptime var codepoints: [utf8.len]Codepoint = undefined;
+    // UTF-8
+
+    pub inline fn fromUtf8Comptime(comptime in: []const u8) Self {
+        comptime var codepoints: [in.len]Codepoint = undefined;
 
         comptime var i: usize = 0;
         comptime var len: usize = 0;
-        inline while (i < utf8.len) : (len += 1) {
-            const codepoint = comptime Codepoint.fromUtf8(utf8[i..]) catch @compileError("invalid UTF-8 in string");
+        inline while (i < in.len) : (len += 1) {
+            const codepoint = comptime Codepoint.fromUtf8(in[i..]) catch @compileError("invalid UTF-8 in string");
             codepoints[len] = codepoint[0];
             i += codepoint[1];
         }
@@ -293,9 +342,9 @@ pub const StaticString = struct {
         return .{ .codepoints = codepoints[0..len] };
     }
 
-    pub fn fromUtf8Alloc(allocator: Allocator, utf8: []const u8) !Self {
+    pub fn fromUtf8Alloc(allocator: Allocator, in: []const u8) !Self {
         return .{
-            .codepoints = (try codepointsFromUtf8Alloc(allocator, utf8)).items,
+            .codepoints = (try codepointsFromUtf8Alloc(allocator, in)).items,
         };
     }
 
@@ -303,15 +352,31 @@ pub const StaticString = struct {
         return codepointsToUtf8Alloc(allocator, self.codepoints);
     }
 
-    pub fn fromUtf16leAlloc(allocator: Allocator, utf16le: []const u16) !Self {
+    // Java Modified UTF-8
+
+    pub fn fromMutf8Alloc(allocator: Allocator, in: []const u8) !Self {
         return .{
-            .codepoints = (try codepointsFromUtf16leAlloc(allocator, utf16le)).items,
+            .codepoints = (try codepointsFromMutf8Alloc(allocator, in)).items,
+        };
+    }
+
+    pub fn toMutf8Alloc(self: *const Self, allocator: Allocator) ![]u8 {
+        return codepointsToMutf8Alloc(allocator, self.codepoints);
+    }
+
+    // UTF-16-LE
+
+    pub fn fromUtf16leAlloc(allocator: Allocator, in: []const u16) !Self {
+        return .{
+            .codepoints = (try codepointsFromUtf16leAlloc(allocator, in)).items,
         };
     }
 
     pub fn toUtf16leAlloc(self: *const Self, allocator: Allocator) ![]u16 {
         return codepointsToUtf16leAlloc(allocator, self.codepoints);
     }
+
+    // OPERATIONS
 
     pub inline fn eq(self: *const Self, other: *const Self) bool {
         if (self.codepoints.len != other.codepoints.len) {
