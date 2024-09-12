403013645071df50e20ed9f412b88f771c611486
satvrn <pastawho@gmail.com>
2024-09-11 17:28:56 -0700

    rename

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("unicode", .{
+    _ = b.addModule("utftools", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
